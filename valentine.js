c840669a351c864f0f930c964354e65737185db7
Fast Man <57269447+MetisMan@users.noreply.github.com>
2024-02-14 02:52:56 -0600

    Update valentine.js

--- valentine.js
+++ valentine.js
@@ -1,31 +1,50 @@
-document.addEventListener('DOMContentLoaded', load);
+// document.addEventListener('DOMContentLoaded', load);
 
-function load() {
-    document.getElementById('yes').addEventListener("click", yesValentine);
-    document.getElementById('no').addEventListener("click", noValentine);
-}
+// function load() {
+//     document.getElementById('yes').addEventListener("click", yesValentine);
+//     document.getElementById('no').addEventListener("click", noValentine);
+// }
 
-function yesValentine() {
-// delete buttons, give big yippee!!
-}
+// function yesValentine() {
+// // delete buttons, give big yippee!!
+// }
 
-function noValentine() {
-var noCount;
-noCount += 1;
+// function noValentine() {
+// var noCount;
+// noCount += 1;
 
-switch(noCount) {
-    case 1:
-        document.getElementById('no').innerHTML = 'please reconsider';
-        break;
-    case 2:
-        document.getElementById('no').innerHTML = 'PLEASE BE MY VALENTINE EMILY?!?!?!?';
-        break;
-    case 3:
-        document.getElementById('no').innerHTML = 'IM NOT GONNA ASK AGAIN BITCH';
-        break;
-    case 4:
-        document.getElementById('no').remove();
-        break;
-}
+// switch(noCount) {
+//     case 1:
+//         document.getElementById('no').innerHTML = 'please reconsider';
+//         break;
+//     case 2:
+//         document.getElementById('no').innerHTML = 'PLEASE BE MY VALENTINE EMILY?!?!?!?';
+//         break;
+//     case 3:
+//         document.getElementById('no').innerHTML = 'IM NOT GONNA ASK AGAIN BITCH';
+//         break;
+//     case 4:
+//         document.getElementById('no').remove();
+//         break;
+// }
 
-}
+// }
+
+
+
+var noButton = document.getElementById('no');
+var noCount = 0;
+noButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    noCount += 1;
+    switch(noCount) {
+        case 1:
+            noButton.innerHTML = 'One';
+            break;
+        case 2:
+            noButton.innerHTML = 'Two';
+            break;
+        default:
+            noButton.innerHTML = 'More than 2';
+        }
+}, false);
