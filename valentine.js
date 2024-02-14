d0d14227efa9339a0396ddb2b21e448959fed076
Fast Man <57269447+MetisMan@users.noreply.github.com>
2024-02-14 02:30:00 -0600

    wsdqw

---
+++ valentine.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', load);
+
+function load() {
+    document.getElementById('yes').addEventListener("click", yesValentine);
+    document.getElementById('no').addEventListener("click", noValentine);
+}
+
+function yesValentine() {
+// delete buttons, give big yippee!!
+}
+
+function noValentine() {
+let noCount;
+noCount++;
+
+switch(noCount) {
+    case 1:
+        document.getElementById('no').innerHTML = 'please reconsider';
+        break;
+    case 2:
+        document.getElementById('no').innerHTML = 'PLEASE BE MY VALENTINE EMILY?!?!?!?';
+        break;
+    case 3:
+        document.getElementById('no').innerHTML = 'IM NOT GONNA ASK AGAIN BITCH';
+        break;
+    case 4:
+        document.getElementById('no').remove();
+        break;
+}
+
+}
